529438550c10deddbe015d4f4cf5ee5dcf378f1e
irene.gonzalez <irene@LAPTOP-JLDIMC46.localdomain>
2022-08-10 14:23:27 +0200

    new branch

--- src/components/header.js
+++ src/components/header.js
@@ -6,20 +6,22 @@ const Header = (props) => {
 	return (
 		<header>
 			<div>
-        <p>Home</p>
+				<p>Home</p>
 				<p>Biografía</p>
 				<p>Proyectos</p>
 				<p>Currículum</p>
-        <p>Contacto</p>
+				<p>Contacto</p>
 			</div>
-      <div>
-        <p>Linkedin</p>
-        <p>GitHub</p>
-      </div>
-			<p>
-				Irene
-				<br /> González
-			</p>
+			<div>
+				<div>
+					<p>Linkedin</p>
+					<p>GitHub</p>
+				</div>
+				<p>
+					Irene
+					<br /> González
+				</p>
+			</div>
 		</header>
 	);
 };
