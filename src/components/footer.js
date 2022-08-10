583f4ae1d6519c2cf144da8a1c2fe7a3ca4fcac8
irene.gonzalez <irene@LAPTOP-JLDIMC46.localdomain>
2022-08-10 13:00:12 +0200

    add header and footer component

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+// STYLESHEET
+import "../styles/variables.css";
+import "../styles/footer.css";
+
+const Footer = (props) => {
+	return (
+		<Footer className="Footer">
+		</Footer>
+	);
+};
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+// STYLESHEET
+import "../styles/variables.css";
+import "../styles/header.css";
+
+const Header = (props) => {
+	return <Header className="Header"></Header>;
+};
+
+export default Header;
