fe09bc48a7fdbdf87eca4e3a61f90ec7736947f8
irene.gonzalez <irene@LAPTOP-JLDIMC46.localdomain>
2022-08-22 13:25:21 +0200

    create data in json

--- src/components/home.js
+++ src/components/home.js
@@ -7,9 +7,24 @@ const Home = (props) => {
 			<section>
 				<section>
 					<h3>pequeño relato</h3>
-					<h2>Breve descripción introductoria</h2>
+					<h2>Programadora junior con un hambre atroz por aprender</h2>
 					<p>
-						cuenta lo que quieras
+						Estudié conservación y restauración de obras de arte, carrera que me aportó grandes cualidades como la
+						resistencia a la frustración, a hacer el trabajo de manera eficiente, trabajar bajo la presión de un tiempo
+						de entrega muy ajustado, a trabajar en equipo aprovechando al máximo las capacidades de cada uno para sacar
+						el mayor partrido.
+					</p>
+					<p>
+						Aunque parezca en un principio que esta experiencia o conocimientos no aportan demasiado he aprendido que
+						son cualidades francamente valiosas en cualquier ámbito laboral.
+					</p>
+					<p>
+						Actualmente me encuentro trabajando como Programadora web junior donde mi función principal es la
+						maquetación, para ello uso HTML5, CSS3, jQuery, gulp y sass.
+					</p>
+					<p>
+						Para saciar mis ganas de aprender hago pequeños proyectos por mi cuenta, tanto en JavaScript Vanilla como en
+						ReactJS.
 					</p>
 				</section>
 			</section>
@@ -26,8 +41,7 @@ const Home = (props) => {
 				<div>
 					<h4>Knit Projects</h4>
 					<p>
-						Web con la que organizar y filtrar tus proyectos tejeriles, aún es una versión primitiva, pero poco a poco
-						irá creciendo
+						Web con la que organizar y filtrar tus proyectos tejeriles, aún es una versión primitiva, pero poco a poco irá creciendo
 					</p>
 				</div>
 				<div>
