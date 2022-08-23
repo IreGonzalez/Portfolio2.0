cadeded29004e6e3367a4a2f252876a4bb510eed
irene.gonzalez <irene@LAPTOP-JLDIMC46.localdomain>
2022-08-23 14:34:33 +0200

    remove style waiting design

--- src/components/header.js
+++ src/components/header.js
@@ -5,22 +5,22 @@ const Header = (props) => {
 	return (
 		<header className="header">
 			<div>
-				<svg className="hamburguer js_ham" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+				<svg className="hamburguer pointer js_ham" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
 					<path d="M120 256C120 286.9 94.93 312 64 312C33.07 312 8 286.9 8 256C8 225.1 33.07 200 64 200C94.93 200 120 225.1 120 256zM280 256C280 286.9 254.9 312 224 312C193.1 312 168 286.9 168 256C168 225.1 193.1 200 224 200C254.9 200 280 225.1 280 256zM328 256C328 225.1 353.1 200 384 200C414.9 200 440 225.1 440 256C440 286.9 414.9 312 384 312C353.1 312 328 286.9 328 256z" />
 				</svg>
-				<nav className="menu js_menu">
-					<a className="menu-element">Home</a>
-					<a className="menu-element">Biografía</a>
-					<a className="menu-element">Proyectos</a>
-					<a className="menu-element">Currículum</a>
-					<a className="menu-element">Contacto</a>
+				<nav className="menu header js_menu">
+					<a className="menu-element pointer">Home</a>
+					<a className="menu-element pointer">Biografía</a>
+					<a className="menu-element pointer">Proyectos</a>
+					<a className="menu-element pointer">Currículum</a>
+					<a className="menu-element pointer">Contacto</a>
 				</nav>
 			</div>
 
-			<div className="social">
+			<div className="header-social">
 				<nav className="social-nav">
 					<a className="social-a" href="https://www.linkedin.com/in/iregonzalez" title="LinkedIn">
-						<svg className="social-link" viewBox="0 0 448 512" >
+						<svg className=" social-link" viewBox="0 0 448 512">
 							<path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
 						</svg>
 					</a>
