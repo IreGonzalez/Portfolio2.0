67c3d90c26b97bbd1aeedb09c03fa4c3b3fd350e
IreGonzalez <irenegonzalezpinilla@gmail.com>
2022-11-15 21:08:54 +0100

    mobile layout header and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,23 @@
 // STYLESHEET
 import "../styles/core/_variables.scss";
+import "../styles/Main.scss";
 //COMPONENTS
 import Rrss from "./rrss";
 //HOOKS
 import { Link } from "react-router-dom";
 const Footer = (props) => {
 	return (
-		<footer>
-			<div>
-				<p>©2022 I love Mixins</p>
-			</div>
-			<nav>
-				<Link to="./">Home</Link>
-				<Link to="./bio">Biografía</Link>
-				<Link to="./projects">Proyectos</Link>
-				<Link to="./curriculum">Currículum</Link>
-				<Link to="./contact">Contacto</Link>
-			</nav>
-			<div>
+		<footer className="footer" id="footer">
 				<Rrss></Rrss>
-				<p>Irene González</p>
+			<nav className="footer__menu">
+				<Link className="footer__menu-link" to="./">Home</Link>
+				<Link className="footer__menu-link" to="./bio">Biografía</Link>
+				<Link className="footer__menu-link" to="./projects">Proyectos</Link>
+				<Link className="footer__menu-link" to="./curriculum">Currículum</Link>
+				<Link className="footer__menu-link" to="./contact">Contacto</Link>
+			</nav>
+			<div className="footer__copy">
+				<p>©2022 I love Mixins</p>
 			</div>
 
 		</footer>
--- src/components/rrss.js
+++ src/components/rrss.js
@@ -1,15 +1,16 @@
 // STYLESHEET
 import "../styles/core/_variables.scss";
+import "../styles/Main.scss";
 
 const Rrss = (props) =>{
     return (
-        <nav>
-            <a href="https://www.linkedin.com/in/iregonzalez" title="LinkedIn">
-						<i class="fa-brands fa-linkedin-in"></i>
-					</a>
-					<a href="https://github.com/IreGonzalez" title="GitHub">
-						<i class="fa-brands fa-github-alt"></i>
-					</a>
+        <nav className="social-nav">
+            <a className="social-link" href="https://www.linkedin.com/in/iregonzalez" title="LinkedIn">
+                <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+            <a className="social-link" href="https://github.com/IreGonzalez" title="GitHub">
+                <i className="fa-brands fa-github-alt"></i>
+            </a>
         </nav>
     );
 }
